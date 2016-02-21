eba936b54b3f14dc490a6a487376177dd7da9ab6
Maddie Ostergaard <mostergaard@stuy.edu>
2016-02-20 20:31:19 -0500

    added main method

--- Question2.java
+++ Question2.java
@@ -5,4 +5,13 @@ public class Question2 {
     // [ 2, 1, 2, 4 ]  is missing 3
     // Compose a function to find the missing number in linear ( O(n) ) run time and using constant ( O(1) ) additional space
     // ( Array modification is allowed, but creating a temporary array is not.)
+    
+    public static int findMissing(int[] list){}
+    
+    public static void main(String[] args){
+	    int[] list = {1,2,2,4};
+	    System.out.println(findMissing(list));
+	    int[] list2 = {1,3,1,4};
+	    System.out.println(findMissing(list2));
+    }
 }
