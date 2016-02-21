10e459c784382db83b4a52cfd2e1270edc9d152b
Maddie Ostergaard <mostergaard@stuy.edu>
2016-02-20 21:39:01 -0500

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -6,7 +6,23 @@ public class Question2 {
     // Compose a function to find the missing number in linear ( O(n) ) run time and using constant ( O(1) ) additional space
     // ( Array modification is allowed, but creating a temporary array is not.)
     
-    public static int findMissing(int[] list){}
+    public static int findMissing(int[] list){
+    	int selectedNum = list[0];
+    	int sortedSection = 1;
+    	int testIndex = 0;
+	while (sortedSection <= list.length){       // each pass 
+	    selectedNum = list[sortedSection]; // mark number to be moved
+	    testIndex = sortedSection - 1;         // mark where it is coming from
+	    while (testIndex >= 0 && selectedNum < list[testIndex]){ // Total bogus |
+		list[testIndex + 1] = list[testIndex];//                            |
+		testIndex--;//                                                      |
+	    }//                                                                     V 
+	    list[testIndex + 1] =  selectedNum; // 
+	    sortedSection++;
+	}
+    		
+    		
+    }
     
     public static void main(String[] args){
 	    int[] list = {1,2,2,4};
