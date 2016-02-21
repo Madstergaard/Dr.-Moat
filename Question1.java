740a2fc1bcb7cc9eb7a6f7e251d98f6ea5cc14f0
Maddie Ostergaard <mostergaard@stuy.edu>
2016-02-20 20:12:15 -0500

    assignment detail

--- Question1.java
+++ Question1.java
@@ -1,3 +1,9 @@
 public class Question1 {
+ // For an n x n matrix of ints, wherein numbers increase across any row (L->R) and down any column...
+    // e.g.,
+    // | 1  3  5 |
+    // | 3  7  8 |
+    // | 5 12 15 |
+    // Compose a O(n) search fxn.
 
 }
